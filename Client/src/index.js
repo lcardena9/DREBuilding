9ac5b45510dff525967de4160efb025f032b6100
Lucas Cardena <lcardena9@gmail.com>
2018-11-22 15:59:04 -0800

    correct favicon

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import rootReducer from './redux/reducers';
 import { BrowserRouter } from 'react-router-dom';
 
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const store = createStore(
-//   rootReducer,
-//   composeEnhancer(applyMiddleware(thunk))
-// );
 ReactDOM.render(
-//   <Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>,
-//   </Provider>,
   document.getElementById('root'));
